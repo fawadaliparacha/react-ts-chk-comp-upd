fc6bd0cbb6686a827f95f3a7d6984289f630c7c9
fawadaliparacha <fawad.paracha@gmail.com>
2023-02-14 14:16:12 +0500

    1 file modified, 1 added

---
+++ store.ts
@@ -0,0 +1,27 @@
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
+import authReducer from './auth/auth.slice';
+import storage from 'redux-persist/lib/storage';
+
+const customizedMiddleware = getDefaultMiddleware({
+  serializableCheck: false,
+});
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => customizedMiddleware,
+});
+
+export const persistor = persistStore(store);
